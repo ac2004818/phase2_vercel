4fc160468e15cd6e8e3f5c96c0b50dd6d1185152
ac2004818 <157595613+ac2004818@users.noreply.github.com>
2024-05-13 11:05:35 +0300

    Update route.js

--- app/api/items/route.js
+++ app/api/items/route.js
@@ -5,6 +5,9 @@ import DataRepository from '../utils/DataRepository';
 const repo = new DataRepository();
 
 export async function GET(request) {
+    const { searchParams } = new URL(request.url);
+    const t = searchParams.get('t');
+    console.log(t);
     try {
         const items = await repo.getItems();
         Response.statusCode = 200;
