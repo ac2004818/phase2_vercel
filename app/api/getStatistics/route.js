b3be3ee90115feb5b3daa90d0bd0633d9f487741
ac2004818 <157595613+ac2004818@users.noreply.github.com>
2024-05-13 11:05:46 +0300

    Update route.js

--- app/api/getStatistics/route.js
+++ app/api/getStatistics/route.js
@@ -4,6 +4,9 @@ import DataRepository from '../utils/DataRepository';
  const repo = new DataRepository();
 
 export async function GET(request) {
+ const { searchParams } = new URL(request.url);
+    const t = searchParams.get('t');
+    console.log(t);
   try {
     
     const year = new Date().getFullYear().toString();
